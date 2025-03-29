50ada0991346690da08023fb502917d94931750b
NikhilNaik23 <nikhilnaiknenavath@gmail.com>
2025-03-29 21:09:07 +0530

    Final Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Removed tailwindcss() as it's not a valid Vite plugin
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
-
+  plugins: [react(),tailwindcss()],
+  server:{
+    port:3000,
+    proxy:{
+      "/api":{
+        target:"http://localhost:5000"
+      }
+    }
+  }
+})
